c2ac848b87c561ae799514bd886c36fefc99539e
Rahul <rituq@LAPTOP-V361DO72>
2024-05-19 09:55:02 +0530

    Added ThreadUsingRunnable class

--- MultiThreading/src/com/multithreading/Main.java
+++ MultiThreading/src/com/multithreading/Main.java
@@ -2,10 +2,15 @@ package com.multithreading;
 
 public class Main {
 	public static void main(String[] args) {
-		ThreadUsingThreadClass thread  = new ThreadUsingThreadClass();
+		/*
+		 * ThreadUsingThreadClass thread = new ThreadUsingThreadClass(); thread.start();
+		 * for(int i = 1; i <= 10; i++) { System.out.println("Main thread"); }
+		 */
+		ThreadUsingRunnable threadUsingRunnable = new ThreadUsingRunnable();
+		Thread thread = new Thread(threadUsingRunnable);
 		thread.start();
-		for(int i = 1; i <= 10; i++) {
-			System.out.println("Main thread");
+		for(int  i = 1; i <= 10; i++) {
+			System.out.println("main thread");
 		}
 		
 	}
---
+++ MultiThreading/src/com/multithreading/ThreadUsingRunnable.java
@@ -0,0 +1,12 @@
+package com.multithreading;
+
+public class ThreadUsingRunnable implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i = 1; i <= 10; i++) {
+			System.out.println("Child thread");
+		}
+	}
+
+}
